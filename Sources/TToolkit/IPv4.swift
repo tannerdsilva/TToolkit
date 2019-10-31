9d330852e4bf467b8de8e1b81664c03ce950e7d8
Tanner Silva <tannerdsilva@Tanners-Mac-mini.local>
2019-10-31 01:41:16 +0000

    IPv4 address structures

---
+++ Sources/TToolkit/IPv4.swift
@@ -0,0 +1,208 @@
+struct CDIRV4 {
+	let address:AddressV4
+	let subnetNumber:UInt8
+	
+	let bytes:UInt32
+	let subnetMask:UInt32
+	
+	init(_ cdirString:String) throws {
+		enum ParseError:Error {
+			case invalidFormat
+		}
+		let splitSubnet = cdirString.split(separator:"/", omittingEmptySubsequences:true).map { String($0) }
+		guard splitSubnet.count == 2 else {
+			print("Unexpected input \(cdirString) for CDIRV4 initializer")
+			throw ParseError.invalidFormat
+		}
+		
+		guard let subnet = UInt8(splitSubnet[1]) else {
+			print("Unexpected input \(cdirString) for CDIRV4 initializer. Subnet cannot be converted into Int8 value")
+			throw ParseError.invalidFormat
+		}
+		guard subnet <= 32 else {
+			throw ParseError.invalidFormat
+		}
+		subnetNumber = subnet
+		
+		address = try AddressV4(splitSubnet[0])
+		bytes = address.addressInteger()
+		
+		subnetMask = (UInt32.max << (32 - subnet))
+	}
+	
+	func asString() -> String {
+		return address.asString() + "/" + String(subnetNumber)
+	}
+}
+
+struct AddressV4:Comparable {
+	enum InitError:Error {
+		case invalidAddressString
+	}
+	
+	let partOne:UInt8
+	let partTwo:UInt8
+	let partThree:UInt8
+	let partFour:UInt8
+	init(_ addressString:String) throws {
+		let components = addressString.split(separator:".")
+		guard components.count == 4 else {
+			print("\(addressString) is not a valid IPv4 address")
+			throw InitError.invalidAddressString
+		}
+		
+		guard let p1 = UInt8(components[0]) else {
+			print("\(components[0]) cannot be initialized as an 8 bit integer")
+			throw InitError.invalidAddressString
+		}
+		
+		guard let p2 = UInt8(components[1]) else {
+			print("\(components[1]) cannot be initialized as an 8 bit integer")
+			throw InitError.invalidAddressString
+		}
+		
+		guard let p3 = UInt8(components[2]) else {
+			print("\(components[1]) cannot be initialized as an 8 bit integer")
+			throw InitError.invalidAddressString
+		}
+
+		guard let p4 = UInt8(components[3]) else {
+			print("\(components[1]) cannot be initialized as an 8 bit integer")
+			throw InitError.invalidAddressString
+		}
+		
+		partOne = p1
+		partTwo = p2
+		partThree = p3
+		partFour = p4
+	}
+	
+	func addressInteger() -> UInt32 {
+		var addressBuffer:UInt32 = UInt32(partOne)
+		addressBuffer = (addressBuffer << 8) | UInt32(partTwo)
+		addressBuffer = (addressBuffer << 8) | UInt32(partThree)
+		addressBuffer = (addressBuffer << 8) | UInt32(partFour)
+		return addressBuffer
+	}
+	
+	func isWithin(cdir:CDIRV4) -> Bool {
+		if ((cdir.bytes & cdir.subnetMask) == (self.addressInteger() & cdir.subnetMask)) { 
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	func asString() -> String {
+		let newString = String(partOne) + "." + String(partTwo) + "." + String(partThree) + "." + String(partFour)
+		return newString
+	}
+	
+	static func < (lhs:AddressV4, rhs:AddressV4) -> Bool {
+		if (lhs.partOne < rhs.partOne) {
+			return true
+		} else if (lhs.partOne > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partTwo < rhs.partTwo) {
+			return true
+		} else if (lhs.partTwo > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partThree < rhs.partThree) {
+			return true
+		} else if (lhs.partTwo > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partFour < rhs.partFour) {
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	static func <= (lhs:AddressV4, rhs:AddressV4) -> Bool {
+		if (lhs.partOne <= rhs.partOne) {
+			return true
+		} else if (lhs.partOne > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partTwo <= rhs.partTwo) {
+			return true
+		} else if (lhs.partTwo > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partThree <= rhs.partThree) {
+			return true
+		} else if (lhs.partTwo > rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partFour <= rhs.partFour) {
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	static func >= (lhs:AddressV4, rhs:AddressV4) -> Bool {
+		if (lhs.partOne >= rhs.partOne) {
+			return true
+		} else if (lhs.partOne < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partTwo >= rhs.partTwo) {
+			return true
+		} else if (lhs.partTwo < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partThree >= rhs.partThree) {
+			return true
+		} else if (lhs.partTwo < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partFour >= rhs.partFour) {
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	static func > (lhs:AddressV4, rhs:AddressV4) -> Bool {
+		if (lhs.partOne > rhs.partOne) {
+			return true
+		} else if (lhs.partOne < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partTwo > rhs.partTwo) {
+			return true
+		} else if (lhs.partTwo < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partThree > rhs.partThree) {
+			return true
+		} else if (lhs.partTwo < rhs.partTwo) {
+			return false
+		}
+		
+		if (lhs.partFour > rhs.partFour) {
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	static func == (lhs:AddressV4, rhs:AddressV4) -> Bool {
+		return (lhs.partOne == rhs.partOne) && (lhs.partTwo == rhs.partTwo) && (lhs.partThree == rhs.partThree) && (lhs.partFour == rhs.partFour)
+	}
+}
