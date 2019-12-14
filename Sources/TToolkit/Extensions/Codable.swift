5a9d81f154265c69970417d9897152d376ae9e0f
Tanner Silva <t@tannersilva.com>
2019-12-13 22:18:41 -0600

    improved extensions

---
+++ Sources/TToolkit/Extensions/Codable.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+fileprivate let encoder = JSONEncoder()
+fileprivate let decoder = JSONDecoder()
+
+extension Encodable { 
+	public func encodeBinaryJSON(file:URL) throws {
+		let binaryData = try encoder.encode(self)
+		try binaryData.write(to:file)
+	}
+}
+
+extension Decodable {
+	public static func decodeBinaryJSON<T>(file:URL, type:T.Type) throws -> T where T:Decodable {
+		let binaryData = try Data(contentsOf:file)
+		return try decoder.decode(T.self, from:binaryData)
+	}
+}
