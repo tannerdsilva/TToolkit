adee6db5e283a29ba8768484eeb33348cb3016e9
Tanner Silva <t@tannersilva.com>
2020-02-25 12:35:44 -0600

    debug

--- Sources/TToolkit/Host/Process.swift
+++ Sources/TToolkit/Host/Process.swift
@@ -21,21 +21,22 @@ public class LoggedProcess:InteractiveProcess {
             guard let self = self else {
                 return
             }
-            self.processQueue.sync {
-            	print("queue entered")
-				let readData = self.stdout.readData(ofLength:self.readLength)
-				if readData.count > 0 {
-					print("found data")
-					readData.withUnsafeBytes({ rawBuffPointer in
-						print("data acquired")
-						let duplicatedData = Data(rawBuffPointer)
-						print("data initialized")
-						print("sync entered")
-						self.stdoutData.append(duplicatedData)
-						print("appended")
-					})
-				}
-            }
+            print("yay things are happening")
+            //self.processQueue.sync {
+//            	print("queue entered")
+//				let readData = self.stdout.readData(ofLength:self.readLength)
+//				if readData.count > 0 {
+//					print("found data")
+//					readData.withUnsafeBytes({ rawBuffPointer in
+//						print("data acquired")
+//						let duplicatedData = Data(rawBuffPointer)
+//						print("data initialized")
+//						print("sync entered")
+//						self.stdoutData.append(duplicatedData)
+//						print("appended")
+//					})
+//				}
+//            }
         }
         
         stderr.readabilityHandler = { [weak self] _ in
