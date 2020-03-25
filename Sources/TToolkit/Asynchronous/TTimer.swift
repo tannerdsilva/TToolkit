a1a92ff52d7464e5573b68ccf27efe98e06eff27
Tanner Silva <t@tannersilva.com>
2020-03-25 14:31:58 -0500

    TTimer is now more robust. Can now be anchored to a reference date (for synchronizing with other timers). Duration can now be changed dynamically and timer will automatically reschedule based on the previous trigger time

--- Sources/TToolkit/Asynchronous/TTimer.swift
+++ Sources/TToolkit/Asynchronous/TTimer.swift
@@ -31,24 +31,63 @@ public class TTimer {
 	
 	private let priority:Priority
 	private let queue:DispatchQueue
-		
+	
+	public let strictMode:Bool
+	public let autoRun:Bool
+	
 	private(set) public var state:TimerState
 	
 	private var timerSource:DispatchSourceTimer? = nil
 	private var lastTriggerDate:DispatchWallTime? = nil
 	
-	private var _timerInterval:DispatchTimeInterval? = nil
+	private var _anchor:Date? = nil
+	public var anchor:Date? {
+		get {
+			return queue.sync {
+				return _anchor
+			}
+		}
+		set {
+			queue.sync {
+				_anchor = newValue
+				if autoRun {
+					_scheduleTimerIfPossible()
+				}
+			}
+		}
+	}
+	//always adjusts with the anchor date into the past
+	private var _anchorAdjustedNowTime:DispatchWallTime {
+		get {
+			let nowTime = DispatchWallTime.now()
+			guard let hasAnchor = _anchor, let hasDuration = _duration else {
+				return nowTime
+			}
+			let timeDelta = hasAnchor.timeIntervalSinceNow
+			let intervalRemainder = timeDelta.truncatingRemainder(dividingBy:hasDuration)
+			return nowTime + intervalRemainder
+		}
+	}
 	
+	private var _timerInterval:DispatchTimeInterval? {
+		get {
+			return _duration?.asDispatchTimeIntervalSeconds()
+		}
+	}
 	private var _duration:Double? = nil
 	public var duration:Double? {
 		get {
-			return _duration
+			return queue.sync {
+				return _duration
+			}
 		}
 		set {
 			let valueToAssign = newValue
 			queue.sync {
 				_duration = valueToAssign
-				rescheduleTimer(duration:_duration, newHandle:_handler, fireNow:false)
+				if autoRun {
+					_scheduleTimerIfPossible()
+				}
 			}
 		}
 	}
@@ -63,13 +102,10 @@ public class TTimer {
 		set {
 			let valueToAssign = newValue
 			queue.sync {
-				if _handler == nil {
-					_handler = valueToAssign
-					rescheduleTimer(duration:_duration, newHandle:_handler, fireNow:false)
-				} else {
-					_handler = valueToAssign
+				_handler = valueToAssign
+				if autoRun {
+					_scheduleTimerIfPossible()
 				}
-				
 			}
 		}
 	}
@@ -79,7 +115,7 @@ public class TTimer {
 		Return value: 	true if the timer was able to be unscheduled
 						false if the there was no timer scheduled
 	*/
-	private func unscheduleTimer() -> Bool {
+	private func _unscheduleTimer() -> Bool {
 		if let hasCurrentTimer = timerSource {
 			hasCurrentTimer.cancel()
 			timerSource = nil
@@ -93,21 +129,40 @@ public class TTimer {
 		Schedules a new timer with a valid duration and handler
 		Assumptions: duration must not be 0
 	*/
-	private func scheduleTimer(duration:Double, handler:TimerHandler, fireNow:Bool) {
-		let newSource = DispatchSource.makeTimerSource(queue:priority.globalConcurrentQueue)
-		let newTimeInterval = duration.asDispatchTimeIntervalSeconds()
-		_timerInterval = newTimeInterval
+	private func _rescheduleTimer(lastTrigger:DispatchWallTime?, fireNow:Bool) {
+		guard let hasDuration = _timerInterval, let hasDoubleDuration = _duration, hasDoubleDuration > 0 else {
+			return
+		}
+		let newSource:DispatchSourceTimer
+		if strictMode {
+			newSource = DispatchSource.makeTimerSource(flags:[.strict], queue:priority.globalConcurrentQueue)
+		} else {
+			newSource = DispatchSource.makeTimerSource(flags:[], queue:priority.globalConcurrentQueue)
+		}
 		newSource.setEventHandler { [weak self] in
 			guard let self = self else {
 				return
 			}
-			self.fire()
+			self._fire(markTime:true)
 		}
-		if fireNow {
-			newSource.schedule(deadline:.now(), repeating:newTimeInterval, leeway:.nanoseconds(0))
+		
+		var baseTime:DispatchWallTime
+		if let hasLastTriggerDate = lastTrigger {
+			let newBaseTime = hasLastTriggerDate + hasDuration
+			let nowTime = DispatchWallTime.now()
+			if fireNow == true && nowTime > newBaseTime {
+				baseTime = nowTime
+			} else {
+				baseTime = newBaseTime
+			}
 		} else {
-			newSource.schedule(deadline:.now() + newTimeInterval, repeating:newTimeInterval, leeway:.nanoseconds(0))
+			if fireNow {
+				baseTime = _anchorAdjustedNowTime
+			} else {
+				baseTime = _anchorAdjustedNowTime + hasDuration
+			}
 		}
+		newSource.schedule(wallDeadline:baseTime, repeating:hasDuration, leeway:.nanoseconds(0))
 		newSource.activate()
 		timerSource = newSource
 		state = .activated
@@ -118,35 +173,47 @@ public class TTimer {
 		This function DOES NOT ASSUME that duration is not 0, therefore, this should be the function that is used when the duration and handler public variables are assigned
 		This function must be called in a synchronized dispatch queue
 	*/
-	private func rescheduleTimer(duration:Double?, newHandle:TimerHandler?, fireNow:Bool) {
-		unscheduleTimer()
+	private func _scheduleTimerIfPossible() {
+		_unscheduleTimer()
 		
-		guard let validDuration = duration, validDuration != 0, let hasHandle = newHandle else {
+		guard _handler != nil, _duration != nil else {
 			return
 		}
 		
-		scheduleTimer(duration:validDuration, handler:hasHandle, fireNow:fireNow)
+		_rescheduleTimer(lastTrigger:lastTriggerDate, fireNow:false)
 	}
 	
-	public init() {
-		state = .canceled
-		
-		let defPri = Priority.`default`
-		
-		self.priority = defPri
-		self.queue = DispatchQueue(label:"com.tannersilva.instance.ttimer.sync", qos:defPri.asDispatchQoS(), target:defPri.globalConcurrentQueue)
+	public init(strict:Bool, autoRun:Bool) {
+		let defaultPriority = Priority.`default`
+		self.priority = defaultPriority
+		self.queue = DispatchQueue(label:"com.tannersilva.instance.ttimer.sync", qos:defaultPriority.asDispatchQoS(), target:defaultPriority.globalConcurrentQueue)
+		self.strictMode = strict
+		self.state = .canceled
+		self.autoRun = autoRun
+	}
+	
+	public func run() {
+		queue.sync {
+			_scheduleTimerIfPossible()
+		}
+	}
+	
+	fileprivate func _fire(markTime:Bool) {
+		if markTime {
+			lastTriggerDate = DispatchWallTime.now()
+		}
+		if let hasHandler = _handler {
+			hasHandler(self)
+		}
 	}
 	
 	public func fire() {
 		queue.sync {
-			lastTriggerDate = DispatchWallTime.now()
-			if let hasHandler = _handler {
-				hasHandler(self)
-			}
+			self._fire(markTime:false)
 		}
 	}
 	
-	public func resume() {
+	public func play() {
 		queue.sync {
 			if state == .suspended, let hasTimer = timerSource {
 				hasTimer.resume()
@@ -155,7 +222,7 @@ public class TTimer {
 		}
 	}
 	
-	public func suspend() {
+	public func pause() {
 		queue.sync {
 			if state == .activated, let hasTimer = timerSource {
 				hasTimer.suspend()
@@ -166,13 +233,13 @@ public class TTimer {
 	
 	public func cancel() {
 		queue.sync {
-			unscheduleTimer()
+			_unscheduleTimer()
 		}
 	}
 	
 	deinit {
 		if state != .canceled {
-			unscheduleTimer()
+			_unscheduleTimer()
 		}
 	}
 }
