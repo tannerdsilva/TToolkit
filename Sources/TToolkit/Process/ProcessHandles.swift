031bf033684f999a659951d8e8608e460d9af337
Tanner Silva <t@tannersilva.com>
2020-04-04 03:29:10 -0500

    testing a new design for io reading from the pipes

--- Sources/TToolkit/Process/ProcessHandles.swift
+++ Sources/TToolkit/Process/ProcessHandles.swift
@@ -17,27 +17,67 @@ import Foundation
 	fileprivate let _pipe = Glibc.pipe(_:)
 #endif
 
+internal typealias ReadHandler = (Data) -> Void
+internal typealias WriteHandler = (ProcessHandle) -> Void
 
 fileprivate let ioThreads = DispatchQueue(label:"com.tannersilva.global.process-handle.io", attributes:[.concurrent])
 fileprivate let ppLocks = DispatchQueue(label:"com.tannersilva.global.process-pipe.sync", attributes:[.concurrent])
 fileprivate let ppInit = DispatchQueue(label:"com.tannersilva.global.process-pipe.init-serial")
 
-internal class ProcessPipes {
-	typealias ReadHandler = (Data) -> Void
-	typealias WriteHandler = (ProcessHandle) -> Void
+fileprivate let prThreads = DispatchQueue(label:"com.tannersilva.global.process-pipe.reader", qos:Priority.highest.asDispatchQoS(), attributes:[.concurrent])
+internal class PipeReader {
 	
-	private let concurrentSchedule:DispatchQueue
+	let internalSync:DispatchQueue
+	let scheduleQueue:DispatchQueue
+	
+	var handleQueue:[ProcessHandle:DispatchSourceProtocol]
+	
+	init() {
+		self.internalSync = DispatchQueue(label:"com.tannersilva.instance.process-pipe.reader.sync", target:prThreads)
+		self.scheduleQueue = DispatchQueue(label:"com.tannersilva.instance.process-pipe.reader.concurrent", qos:Priority.high.asDispatchQoS(), attributes:[.concurrent], target:prThreads)
+		self.handleQueue = [ProcessHandle:DispatchSourceProtocol]()
+	}
+	
+	func scheduleForReading(_ handle:ProcessHandle, queue:DispatchQueue, work:@escaping(ReadHandler)) {
+		let newSource = DispatchSource.makeReadSource(fileDescriptor:handle.fileDescriptor, queue:scheduleQueue)
+		newSource.setEventHandler {
+			if let newData = handle.availableData() {
+				let workItem = DispatchWorkItem(flags:[.inheritQoS]) {
+					work(newData)
+				}
+				queue.async(execute:workItem)
+			}
+		}
+		internalSync.sync {
+			if let hasExisting = handleQueue[handle] {
+				hasExisting.cancel()
+			}
+			handleQueue[handle] = newSource
+			newSource.activate()
+		}
+		print(Colors.cyan("[\(handle.fileDescriptor)] scheduled for reading."))
+	}
+	
+	func unschedule(_ handle:ProcessHandle) {
+		internalSync.sync {
+			if let hasExisting = handleQueue[handle] {
+				hasExisting.cancel()
+				handleQueue[handle] = nil
+			}
+		}
+		print(Colors.magenta("[\(handle.fileDescriptor)] unscheduled"))
+	}
+}
+internal let globalPR = PipeReader()
+
+
+internal class ProcessPipes {
 	private let internalSync:DispatchQueue
 	private let internalCallback:DispatchQueue
 	
 	let reading:ProcessHandle
 	let writing:ProcessHandle
-	
-	//MARK: Handlers
-	//these are the timing sources that handle the readability and writability handler 
-	private var writeSource:DispatchSourceProtocol? = nil
-	private var readSource:DispatchSourceProtocol? = nil
-	
+
 	private var _callbackQueue:DispatchQueue
 	var handlerQueue:DispatchQueue {
 		get {
@@ -63,80 +103,56 @@ internal class ProcessPipes {
 		}
 		set {
 			internalSync.sync {
-				//cancel the old handler source if it exists
-				if let hasReadSource = readSource {
-					hasReadSource.cancel()
-				}
-				
-				//if there is a new handler to schedule...
 				if let hasNewHandler = newValue {
 					_readHandler = hasNewHandler
-
-					//schedule the new timer
-					let newSource = DispatchSource.makeReadSource(fileDescriptor:reading.fileDescriptor, queue:concurrentSchedule)
-					let reader = reading
-					let intCbQueue = internalCallback
-					newSource.setEventHandler { [weak self] in
-						guard let self = self else {
-							return
-						}
-						self.internalSync.sync {
-							if let newData = reader.availableData() {
-								intCbQueue.async {
-									hasNewHandler(newData)
-								}
-							}
-						}
-					}
-					readSource = newSource
-					newSource.activate()
+					globalPR.scheduleForReading(reading, queue:internalCallback, work:hasNewHandler)
 				} else {
 					_readHandler = nil
-					readSource = nil
+					globalPR.unschedule(reading)
 				}
 			}
 		}
 	}
 	
-	//write handler
-	private var _writeHandler:WriteHandler? = nil
-	var writeHandler:WriteHandler? {
-		get {
-			return internalSync.sync {
-				return _writeHandler
-			}
-		}
-		set {
-			internalSync.sync {
-				//cancel the existing writing source if it exists
-				if let hasWriteSource = writeSource {
-					hasWriteSource.cancel()
-				}
-				//assign the new value and schedule a new writing source if necessary
-				if let hasNewHandler = newValue {
-					_writeHandler = hasNewHandler
-					
-					//schedule the new timer
-					let newSource = DispatchSource.makeWriteSource(fileDescriptor:writing.fileDescriptor, queue:concurrentSchedule)
-					let writer = writing
-					let intCbQueue = internalCallback
-					newSource.setEventHandler { [weak self] in
-						intCbQueue.async { [weak self] in
-							guard let self = self else {
-								return
-							}
-							hasNewHandler(self.writing)
-						}
-					}
-					writeSource = newSource
-					newSource.activate()
-				} else {
-					_writeHandler = nil
-					writeSource = nil
-				}
-			}
-		}
-	}
+//	write handler
+//	private var _writeHandler:WriteHandler? = nil
+//	var writeHandler:WriteHandler? {
+//		get {
+//			return internalSync.sync {
+//				return _writeHandler
+//			}
+//		}
+//		set {
+//			internalSync.sync {
+//				cancel the existing writing source if it exists
+//				if let hasWriteSource = writeSource {
+//					hasWriteSource.cancel()
+//				}
+//				assign the new value and schedule a new writing source if necessary
+//				if let hasNewHandler = newValue {
+//					_writeHandler = hasNewHandler
+//					
+//					schedule the new timer
+//					let newSource = DispatchSource.makeWriteSource(fileDescriptor:writing.fileDescriptor, queue:concurrentSchedule)
+//					let writer = writing
+//					let intCbQueue = internalCallback
+//					newSource.setEventHandler { [weak self] in
+//						intCbQueue.async { [weak self] in
+//							guard let self = self else {
+//								return
+//							}
+//							hasNewHandler(self.writing)
+//						}
+//					}
+//					writeSource = newSource
+//					newSource.activate()
+//				} else {
+//					_writeHandler = nil
+//					writeSource = nil
+//				}
+//			}
+//		}
+//	}
 	
 	init(callback:DispatchQueue) throws {
 		let readWrite = try Self.forReadingAndWriting()
@@ -144,7 +160,6 @@ internal class ProcessPipes {
 		self.reading = readWrite.r
 		self.writing = readWrite.w
 		
-		self.concurrentSchedule = DispatchQueue(label:"com.tannersilva.instance.process-pipe.schedule", qos:Priority.high.asDispatchQoS(), target:ioThreads)
 		let ints = DispatchQueue(label:"com.tannersilva.instance.process-pipe.sync", target:ppLocks)
 		self.internalSync = ints
 		let icb = DispatchQueue(label:"com.tannersilva.instance.process-pipe.callback", target:callback)
@@ -176,36 +191,47 @@ internal class ProcessPipes {
 		reading.close()
 		writing.close()
 		readHandler = nil
-		writeHandler = nil
 	}
 	
 	deinit {
 		readHandler = nil
-		writeHandler = nil
 	}
 }
 
-fileprivate let phLock = DispatchQueue(label:"com.tannersilva.global.process-handle.sync", qos:Priority.low.asDispatchQoS(), attributes:[.concurrent])
-
-internal class ProcessHandle {
+fileprivate let sq = DispatchQueue(label:"com.tannersilva.global.process-handle.sync", attributes:[.concurrent])
+internal class ProcessHandle:Hashable {
 	fileprivate let internalSync:DispatchQueue
 	
-	private var _fd:Int32
+	private var _isClosed:Bool
+	var isClosed:Bool {
+		get {
+			return internalSync.sync {
+				return _isClosed
+			}
+		}
+	}
+	
+	private let _fd:Int32
 	var fileDescriptor:Int32 {
 		get {
-			return _fd
+			internalSync.sync {
+				return _fd
+			}
 		}
 	}
 	
 	init(fd:Int32) {
 		self._fd = fd
-		self.internalSync = DispatchQueue(label:"com.tannersilva.instance.process-handle.sync", qos:Priority.low.asDispatchQoS(), target:phLock)
+		self.internalSync = DispatchQueue(label:"com.tannersilva.instance.process-handle.sync", target:sq)
+		self._isClosed = false
 	}
 	
 	func write(_ dataObj:Data) throws {
-		let start = Date()
 		try internalSync.sync {
-			print(Colors.dim("PH syncronized in \(start.timeIntervalSinceNow) seconds"))
+			guard _isClosed != true else {
+				//should throw
+				return
+			}
 			try dataObj.withUnsafeBytes({
 				if let hasBaseAddress = $0.baseAddress {
 					try write(buf:hasBaseAddress, length:dataObj.count)
@@ -233,6 +259,9 @@ internal class ProcessHandle {
 		let start = Date()
 		return internalSync.sync {
 			print(Colors.dim("PH syncronized in \(start.timeIntervalSinceNow) seconds"))
+			guard _isClosed != true else {
+				return nil
+			}
 			var statbuf = stat()
 			if fstat(_fd, &statbuf) < 0 {
 				return nil
@@ -265,19 +294,27 @@ internal class ProcessHandle {
 		let start = Date()
 		internalSync.sync {
 			print(Colors.dim("PH syncronized in \(start.timeIntervalSinceNow) seconds"))
-			guard _fd != -1 else {
+			guard _isClosed != true else {
 				return
 			}
 		
 			guard _close(_fd) >= 0 else {
 				return
 			}
-			_fd = -1
+			_isClosed = true
 		}
 	}
 	
+	func hash(into hasher:inout Hasher) {
+		hasher.combine(_fd)
+	}
+	
+	static func == (lhs:ProcessHandle, rhs:ProcessHandle) -> Bool {
+		return lhs._fd == rhs._fd
+	}
+	
 	deinit {
-		if _fd != -1 {
+		if isClosed == false {
 			_ = _close(_fd)
 		}
 	}
