8f9ac2c2f0fea207392d83806133d89975992cd2
Tanner Silva <t@tannersilva.com>
2020-04-06 23:58:43 -0500

    i think I'm pretty clever

--- Sources/TToolkit/Asynchronous/Explode.swift
+++ Sources/TToolkit/Asynchronous/Explode.swift
@@ -12,6 +12,7 @@ import Foundation
 extension Collection {
 	//explode a collection - no return values	
 	public func explode(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> Void) {
+		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
 		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
 		var iterator = makeIterator()
 		
@@ -21,9 +22,11 @@ extension Collection {
 			}
 		}
 		
-		DispatchQueue.concurrentPerform(iterations:lanes) { n in
-			while let curItem = popItem() {
-				try? thisFunction(n, curItem)
+		concurrent.sync {
+			DispatchQueue.concurrentPerform(iterations:lanes) { n in
+				while let curItem = popItem() {
+					try? thisFunction(n, curItem)
+				}
 			}
 		}
 	}
@@ -31,6 +34,7 @@ extension Collection {
 	//explode a collection - allows the user to handle the merging of data themselves.
 	//return values of the primary `explode` block are passed to a serial thread where the user can handle the data as necessary	
 	public func explode<T>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> T?, merge mergeFunction:@escaping (Int, T) throws -> Void) {
+		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
 		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
 		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
 		var iterator = makeIterator()
@@ -41,20 +45,22 @@ extension Collection {
 			}
 		}
 		
-		DispatchQueue.concurrentPerform(iterations:lanes) { n in
-			while let curItem = popItem() {
-				if let returnedValue = try? thisFunction(n, curItem) {
-					mergeSync.sync {
-						try? mergeFunction(n, returnedValue)
+		concurrent.sync {
+			DispatchQueue.concurrentPerform(iterations:lanes) { n in
+				while let curItem = popItem() {
+					if let returnedValue = try? thisFunction(n, curItem) {
+						mergeSync.sync {
+							try? mergeFunction(n, returnedValue)
+						}
 					}
 				}
 			}
 		}
 	}
 
-
 	//explode a collection - returns a set of hashable objects
 	public func explode<T>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> T?) -> Set<T> where T:Hashable {	
+		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
 		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
 		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
 		var iterator = makeIterator()
@@ -66,11 +72,13 @@ extension Collection {
 			}
 		}
 		
-		DispatchQueue.concurrentPerform(iterations:lanes) { n in
-			while let curItem = popItem() {
-				if let returnedValue = try? thisFunction(n, curItem) {
-					mergeSync.sync {
-						_ = buildData.update(with:returnedValue)
+		concurrent.sync {
+			DispatchQueue.concurrentPerform(iterations:lanes) { n in
+				while let curItem = popItem() {
+					if let returnedValue = try? thisFunction(n, curItem) {
+						mergeSync.sync {
+							_ = buildData.update(with:returnedValue)
+						}
 					}
 				}
 			}
@@ -81,6 +89,7 @@ extension Collection {
 
 	//explode a collection - returns a dictionary
 	public func explode<T, U>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> (key:T, value:U)) -> [T:U] where T:Hashable {
+		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
 		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
 		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
 		var iterator = makeIterator()
@@ -92,12 +101,14 @@ extension Collection {
 			}
 		}
 		
-		DispatchQueue.concurrentPerform(iterations:lanes) { n in
-			while let curItem = popItem() {
-				if let returnedValue = try? thisFunction(n, curItem) {
-					if returnedValue.value != nil {
-						mergeSync.sync {
-							buildData[returnedValue.key] = returnedValue.value
+		concurrent.sync {
+			DispatchQueue.concurrentPerform(iterations:lanes) { n in
+				while let curItem = popItem() {
+					if let returnedValue = try? thisFunction(n, curItem) {
+						if returnedValue.value != nil {
+							mergeSync.sync {
+								buildData[returnedValue.key] = returnedValue.value
+							}
 						}
 					}
 				}
