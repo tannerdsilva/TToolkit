5a15345c7440d2ca1f13bb936df58406db3f1e68
Tanner Silva <t@tannersilva.com>
2020-04-07 00:26:40 -0500

    trying something different with explode

--- Sources/TToolkit/Asynchronous/Explode.swift
+++ Sources/TToolkit/Asynchronous/Explode.swift
@@ -9,130 +9,123 @@ import Foundation
 
 //explosions allow for multi-threaded collection mapping
 
-		let globemerge = DispatchQueue(label:"com.tannersilva.global.function.explode.merge")
-
 extension Collection {
-	//explode a collection - no return values	
+	//explode a collection - no return values
 	public func explode(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> Void) {
+		let semaphore = DispatchSemaphore(value:lanes)
+		let computeThread = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
+		let queueingGroup = DispatchGroup()
 		let flightGroup = DispatchGroup()
-		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
-		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
-		var iterator = makeIterator()
-		
-		func popItem() -> Element? {
-			return internalSync.sync {
-				iterator.next()
+		for (n, curItem) in enumerated() {
+			queueingGroup.enter()
+			computeThread.async {
+				flightGroup.enter()
+				semaphore.wait()
+				queueingGroup.leave()
+				do {
+					try thisFunction(n, curItem)
+				} catch _ {}
+				semaphore.signal()
+				flightGroup.leave()
 			}
-		}
-		
-		flightGroup.enter()
-		concurrent.async {
-			DispatchQueue.concurrentPerform(iterations:lanes) { n in
-				globemerge.sync { print("\(n)") }
-				while let curItem = popItem() {
-					try? thisFunction(n, curItem)
-				}
-			}
-			flightGroup.leave()
+			queueingGroup.wait()
 		}
 		flightGroup.wait()
 	}
 
 	//explode a collection - allows the user to handle the merging of data themselves.
-	//return values of the primary `explode` block are passed to a serial thread where the user can handle the data as necessary	
+	//return values of the primary `explode` block are passed to a serial thread where the user can handle the data as necessary
 	public func explode<T>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> T?, merge mergeFunction:@escaping (Int, T) throws -> Void) {
+		let semaphore = DispatchSemaphore(value:lanes)
+		let computeThread = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
+		let mergeQueue = DispatchQueue(label:"com.tannersilva.function.explode.sync", target:computeThread)
+		let queueingGroup = DispatchGroup()
 		let flightGroup = DispatchGroup()
-		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
-		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
-		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
-		var iterator = makeIterator()
-		
-		func popItem() -> Element? {
-			return internalSync.sync {
-				iterator.next()
-			}
-		}
-		
-		flightGroup.enter()
-		concurrent.async {
-			DispatchQueue.concurrentPerform(iterations:lanes) { n in
-				globemerge.sync { print("\(n)") }
-				while let curItem = popItem() {
-					if let returnedValue = try? thisFunction(n, curItem) {
-						mergeSync.sync {
+		for (n, curItem) in enumerated() {
+			queueingGroup.enter()
+			computeThread.async {
+				flightGroup.enter()
+				semaphore.wait()
+				queueingGroup.leave()
+				do {
+					if let returnedValue = try thisFunction(n, curItem) {
+						mergeQueue.sync {
 							try? mergeFunction(n, returnedValue)
 						}
 					}
-				}
+				} catch _ {}
+				semaphore.signal()
+				flightGroup.leave()
 			}
-			flightGroup.leave()
+			queueingGroup.wait()
 		}
+		flightGroup.wait()
 	}
 
 	//explode a collection - returns a set of hashable objects
-	public func explode<T>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> T?) -> Set<T> where T:Hashable {	
+	public func explode<T>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> T?) -> Set<T> where T:Hashable {
+		let semaphore = DispatchSemaphore(value:lanes)
+		let computeThread = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
+		let mergeQueue = DispatchQueue(label:"com.tannersilva.function.explode.sync", target:computeThread)
+		let queueingGroup = DispatchGroup()
 		let flightGroup = DispatchGroup()
-		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
-		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
-		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
-		var iterator = makeIterator()
+	
 		var buildData = Set<T>()
-		
-		func popItem() -> Element? {
-			return internalSync.sync {
-				iterator.next()
-			}
-		}
-		
-		flightGroup.enter()
-		concurrent.sync {
-			DispatchQueue.concurrentPerform(iterations:lanes) { n in
-				globemerge.sync { print("\(n)") }
-				while let curItem = popItem() {
-					if let returnedValue = try? thisFunction(n, curItem) {
-						mergeSync.sync {
+	
+		for (n, curItem) in enumerated() {
+			queueingGroup.enter()
+			computeThread.async {
+				flightGroup.enter()
+				semaphore.wait()
+				queueingGroup.leave()
+			
+				do {
+					if let returnedValue = try thisFunction(n, curItem) {
+						mergeQueue.sync {
 							_ = buildData.update(with:returnedValue)
 						}
 					}
-				}
+				} catch _ {}
+			
+				semaphore.signal()
+				flightGroup.leave()
 			}
-			flightGroup.leave()
+			queueingGroup.wait()
 		}
+	
 		flightGroup.wait()
 		return buildData
 	}
 
-
 	//explode a collection - returns a dictionary
 	public func explode<T, U>(lanes:Int = ProcessInfo.processInfo.activeProcessorCount, qos:Priority = .`default`, using thisFunction:@escaping (Int, Element) throws -> (key:T, value:U)) -> [T:U] where T:Hashable {
+		let semaphore = DispatchSemaphore(value:lanes)
+		let computeThread = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
+		let mergeQueue = DispatchQueue(label:"com.tannersilva.function.explode.sync", target:computeThread)
+		let queueingGroup = DispatchGroup()
 		let flightGroup = DispatchGroup()
-		let concurrent = DispatchQueue(label:"com.tannersilva.function.explode.async", qos:qos.asDispatchQoS(), attributes:[.concurrent])
-		let internalSync = DispatchQueue(label:"com.tannersilva.function.explode.sync")
-		let mergeSync = DispatchQueue(label:"com.tannersilva.function.explode.merge")
-		var iterator = makeIterator()
+	
 		var buildData = [T:U]()
-		
-		func popItem() -> Element? {
-			return internalSync.sync {
-				iterator.next()
-			}
-		}
-		
-		flightGroup.enter()
-		concurrent.sync {
-			DispatchQueue.concurrentPerform(iterations:lanes) { n in
-				globemerge.sync { print("\(n)") }
-				while let curItem = popItem() {
-					if let returnedValue = try? thisFunction(n, curItem) {
-						if returnedValue.value != nil {
-							mergeSync.sync {
-								buildData[returnedValue.key] = returnedValue.value
-							}
+		for (n, curItem) in enumerated() {
+			queueingGroup.enter()
+			computeThread.async {
+				flightGroup.enter()
+				semaphore.wait()
+				queueingGroup.leave()
+			
+				do {
+					let returnedValue = try thisFunction(n, curItem)
+					if returnedValue.value != nil {
+						mergeQueue.sync {
+							buildData[returnedValue.key] = returnedValue.value
 						}
 					}
-				}
+				} catch _ {}
+			
+				semaphore.signal()
+				flightGroup.leave()
 			}
-			flightGroup.leave()
+			queueingGroup.wait()
 		}
 		flightGroup.wait()
 		return buildData
