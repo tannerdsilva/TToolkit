0fba05f4b690ee18d5ed46373c463829224c74fd
Tanner Silva <t@tannersilva.com>
2020-04-09 00:33:56 -0500

    async run

--- Sources/TToolkit/Process/Queues.swift
+++ Sources/TToolkit/Process/Queues.swift
@@ -43,5 +43,8 @@ extension Priority {
 
 internal let global_lock_queue = DispatchQueue(label:"com.tannersilva.global.process.sync", attributes:[.concurrent])
 
+//this queue is assigned a priority since it will be passed to a dispatchsource
 internal let process_read_fast_capture = DispatchQueue(label:"com.tannersilva.global.process.read.capture", qos:Priority.highest.process_reading_fast_capture_priority, attributes:[.concurrent], target:Priority.highest.globalConcurrentQueue)
+
+internal let process_launch_async_fast = DispatchQueue(label:"com.tannersilva.global.process.launch-serial", qos:maximumPriority, target:process_master_queue)
 
