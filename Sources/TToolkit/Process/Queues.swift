2c1b232d8a0832683309de227c1b8c17e7458825
Tanner Silva <t@tannersilva.com>
2020-04-09 00:23:49 -0500

    process prioriries extesnion

---
+++ Sources/TToolkit/Process/Queues.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+internal let process_master_queue = DispatchQueue(label:"com.tannersilva.global.process", qos:maximumPriority, attributes:[.concurrent])
+
+extension Priority {
+	internal var process_launch_priority:DispatchQoS {
+		get {
+			return self.asDispatchQoS(relative:-20)
+		}
+	}
+	
+	internal var process_callback_priority:DispatchQoS {
+		get {
+			return self.asDispatchQoS(relative:0)
+		}
+	}
+	
+	
+	internal var process_reading_fast_capture_priority:DispatchQoS {
+		get {
+			return self.asDispatchQoS(relative:Int(Int32.max))
+		}
+	}
+	
+	internal var process_reading_priority:DispatchQoS {
+		get {
+			return self.asDispatchQoS(relative:100)
+		}
+	}
+	
+	internal var process_writing_priority:DispatchQoS {
+		get {
+			return self.asDispatchQoS(relative:50)
+		}
+	}
+    
+    internal var process_terminate_priority:DispatchQoS {
+        get {
+            return self.asDispatchQoS(relative:300)
+        }
+    }
+}
+
+internal let global_lock_queue = DispatchQueue(label:"com.tannersilva.global.process.sync", attributes:[.concurrent])
+
+internal let process_read_fast_capture = DispatchQueue(label:"com.tannersilva.global.process.read.capture", qos:Priority.highest.process_reading_fast_capture_priority, attributes:[.concurrent], target:Priority.highest.globalConcurrentQueue)
+
