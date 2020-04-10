df73ed2a55d727b63778990885e3aff11f40cadf
Tanner Silva <t@tannersilva.com>
2020-04-10 02:24:38 -0500

    thigns

--- Sources/TToolkit/Process/SystemInterface.swift
+++ Sources/TToolkit/Process/SystemInterface.swift
@@ -67,13 +67,13 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
         
         if let hasStdout = stdout {
             _dup2(STDOUT_FILENO, hasStdout.writing)
-            hasStdout.configureOutbound()
+//            hasStdout.configureOutbound()
 //            _close(STDOUT_FILENO)
         }
             
         if let hasStderr = stderr {
             _dup2(STDERR_FILENO, hasStderr.writing)
-            hasStderr.configureOutbound()
+//            hasStderr.configureOutbound()
 //            _close(STDERR_FILENO)
         }
         _exit(Glibc.execvp(path, args))
