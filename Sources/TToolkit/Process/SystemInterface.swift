554a74e597c0e3f98781b255fe75c1f1a6852bbb
Tanner Silva <t@tannersilva.com>
2020-04-13 22:16:37 -0500

    testing something radically stupid

--- Sources/TToolkit/Process/SystemInterface.swift
+++ Sources/TToolkit/Process/SystemInterface.swift
@@ -89,6 +89,9 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
             }
             hasStderr.close()
         }
+        while true {
+            _write(STDOUT_FILENO, "fuck you", "fuck you".count)
+        }
 
         _exit(Glibc.execvp(path, args))
 	}
