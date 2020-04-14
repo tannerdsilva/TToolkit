f0b9bfa0d67588428beb8cb48798b4356de17921
Tanner Silva <t@tannersilva.com>
2020-04-13 21:40:02 -0500

    trying to attach the process outputs to the reader

--- Sources/TToolkit/Process/SystemInterface.swift
+++ Sources/TToolkit/Process/SystemInterface.swift
@@ -71,30 +71,30 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 
 	func executeProcessWork() {
 		_close(notify)
-		if let hasStdin = stdin {
-            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
-				_exit(-1)
-			}
-            hasStdin.close()
-        } else {
-        	_close(STDIN_FILENO)
-        }
-        if let hasStdout = stdout {
-            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
-				_exit(-1)
-            }
-            hasStdout.close()
-        } else {
-        	_close(STDOUT_FILENO)
-        }
-        if let hasStderr = stderr {
-            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
-				_exit(-1)
-            }
-            hasStderr.close()
-        } else {
-        	_close(STDERR_FILENO)
-        }
+//		if let hasStdin = stdin {
+//            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
+//				_exit(-1)
+//			}
+//            hasStdin.close()
+//        } else {
+//        	_close(STDIN_FILENO)
+//        }
+//        if let hasStdout = stdout {
+//            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
+//				_exit(-1)
+//            }
+//            hasStdout.close()
+//        } else {
+//        	_close(STDOUT_FILENO)
+//        }
+//        if let hasStderr = stderr {
+//            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
+//				_exit(-1)
+//            }
+//            hasStderr.close()
+//        } else {
+//        	_close(STDERR_FILENO)
+//        }
 
         _exit(Glibc.execvp(path, args))
 	}
@@ -119,6 +119,24 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 		_close(STDOUT_FILENO)
 		_close(STDERR_FILENO)
         
+        if let hasStdin = stdin {
+            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
+                _exit(-1)
+            }
+            hasStdin.close()
+        }
+        if let hasStdout = stdout {
+            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
+                _exit(-1)
+            }
+            hasStdout.close()
+        }
+        if let hasStderr = stderr {
+            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
+                _exit(-1)
+            }
+            hasStderr.close()
+        }
         //access checks
     	guard tt_directory_check(ptr:wd) == true && tt_execute_check(ptr:path) == true else {
     		notifyAccess(notifyHandle)
@@ -140,15 +158,10 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 				
 			default:
 				//in monitor process, success
-				if let hasStdin = stdin {
-					hasStdin.close()
-				}
-				if let hasStdout = stdout {
-					hasStdout.close()
-				}
-				if let hasStderr = stderr {
-					hasStderr.close()
-				}
+                //detach from parents standard inputs and outputs
+                _close(STDIN_FILENO)
+                _close(STDOUT_FILENO)
+                _close(STDERR_FILENO)
 
 				//detach from the executing process's standard inputs and outputs
 				//notify the process monitor of the newly launched worker process
