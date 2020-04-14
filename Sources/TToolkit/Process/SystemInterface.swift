81b6d7800fc039dde45824d6ef7b9034b0a1ed98
Tanner Silva <t@tannersilva.com>
2020-04-14 10:50:06 -0500

    need to fix the forking problem

--- Sources/TToolkit/Process/SystemInterface.swift
+++ Sources/TToolkit/Process/SystemInterface.swift
@@ -72,6 +72,24 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 	func executeProcessWork() {
 		_close(notify)
 
+                if let hasStdin = stdin {
+                    guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
+                        _exit(-1)
+                    }
+        //            hasStdin.close()
+                }
+                if let hasStdout = stdout {
+                    guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
+                        _exit(-1)
+                    }
+        //            hasStdout.close()
+                }
+                if let hasStderr = stderr {
+                    guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
+                        _exit(-1)
+                    }
+        //            hasStderr.close()
+                }
         for i in 0..<10000 {
             write(stdout!.writing, "fuck you\n", "fuck you\n".count)
         }
@@ -103,24 +121,6 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 		guard chdir(wd) == 0 else {
 			notifyFatal(notifyHandle)
 		
-        if let hasStdin = stdin {
-            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
-                _exit(-1)
-            }
-//            hasStdin.close()
-        }
-        if let hasStdout = stdout {
-            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
-                _exit(-1)
-            }
-//            hasStdout.close()
-        }
-        if let hasStderr = stderr {
-            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
-                _exit(-1)
-            }
-//            hasStderr.close()
-        }
         
        	let processForkResult = fork()
 		switch processForkResult {
