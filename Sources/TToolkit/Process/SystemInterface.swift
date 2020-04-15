f5f2232d5caa11fd8fad7542c039b9a7feeb9f92
Tanner Silva <t@tannersilva.com>
2020-04-14 20:33:44 -0500

    more fixes

--- Sources/TToolkit/Process/SystemInterface.swift
+++ Sources/TToolkit/Process/SystemInterface.swift
@@ -98,6 +98,8 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
         let notifyHandle = ProcessHandle(fd:notify.writing)
         
         try! notifyHandle.write("go fuck yourself only once though hahah")
+        _write(stdout!.writing, "foooooooooooooo gooooo mooooooooo", "foooooooooooooo gooooo mooooooooo".count)
+        print("hooray we wrote stuff")
         
         //access checks
     	guard tt_directory_check(ptr:wd) == true && tt_execute_check(ptr:path) == true else {
@@ -113,29 +115,29 @@ internal func tt_spawn(path:UnsafePointer<Int8>, args:UnsafeMutablePointer<Unsaf
 		guard chdir(wd) == 0 else {
 			notifyFatal(notifyHandle)
         }
-		if let hasStdin = stdin {
-//            _close(hasStdin.writing)?
-            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
-                notifyFatal(notifyHandle)
-            }
-//            _close(hasStdin.reading)
-        }
-        if let hasStderr = stderr {
-            print("err is triggered")
-//            _close(hasStderr.reading)
-            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
-                notifyFatal(notifyHandle)
-            }
-//            _close(hasStderr.writing)
-        }
-        if let hasStdout = stdout {
-            print("out is triggered")
-//            _close(hasStdout.reading)
-            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
-                notifyFatal(notifyHandle)
-            }
-//            _close(hasStdout.writing)
-        }
+//		if let hasStdin = stdin {
+////            _close(hasStdin.writing)?
+//            guard _dup2(hasStdin.reading, STDIN_FILENO) == 0 else {
+//                notifyFatal(notifyHandle)
+//            }
+////            _close(hasStdin.reading)
+//        }
+//        if let hasStderr = stderr {
+//            print("err is triggered")
+////            _close(hasStderr.reading)
+//            guard _dup2(hasStderr.writing, STDERR_FILENO) == 0 else {
+//                notifyFatal(notifyHandle)
+//            }
+////            _close(hasStderr.writing)
+//        }
+//        if let hasStdout = stdout {
+//            print("out is triggered")
+////            _close(hasStdout.reading)
+//            guard _dup2(hasStdout.writing, STDOUT_FILENO) == 0 else {
+//                notifyFatal(notifyHandle)
+//            }
+////            _close(hasStdout.writing)
+//        }
 
        	let processForkResult = fork()
         
