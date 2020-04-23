9cbb2e00136aec841abb53ba0a5de35632a0c47e
Tanner Silva <t@tannersilva.com>
2020-04-22 22:13:10 -0500

    fuck it no target for accessSync

--- Sources/TToolkit/Process/ProcessHandles.swift
+++ Sources/TToolkit/Process/ProcessHandles.swift
@@ -172,7 +172,7 @@ internal class PipeReader {
         
         self.instanceMaster = DispatchQueue(label:"com.tannersilva.instance.pipe.read.master", attributes:[.concurrent], target:process_master_queue)
     
-        self.accessSync = DispatchQueue(label:"com.tannersilva.global.pipe.handle.access.sync", attributes:[.concurrent], target:self.master)
+        self.accessSync = DispatchQueue(label:"com.tannersilva.global.pipe.handle.access.sync", attributes:[.concurrent])
 	}
     
 	internal func readHandle(_ handle:Int32) {
