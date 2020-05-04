682b360273199f5ec0fe58e92a251fe0120be5ac
Tanner Silva <t@tannersilva.com>
2020-05-04 16:28:39 -0500

    trying a new way to read the file handles

--- Sources/TToolkit/Process/ProcessHandles.swift
+++ Sources/TToolkit/Process/ProcessHandles.swift
@@ -53,26 +53,18 @@ extension IODescriptor {
         return Data(bytes:bytesBound, count:amountRead)
     }
     
-    func availableDataLoop() -> Data? {
+    func availableDataLoop(_ outputFunction:(Data?) -> Void) {
         defer {
             print(Colors.Red("Data loop ended"))
         }
-        var tempBuff = Data()
         var i = 0
         while let curData = self.availableData(), curData.count > 0 {
         	i += 1
-            tempBuff.append(curData)
+            outputFunction(curData)
         }
         if i > 1 {
         	print(Colors.bgWhite("Data loop itterated \(i) times"))
         }
-        
-        if tempBuff.count > 0 {
-            return tempBuff
-        } else {
-            return nil
-        }
-        
     }
 }
 
@@ -214,9 +206,12 @@ internal class PipeReader {
         let newSource = DispatchSource.makeReadSource(fileDescriptor:handle, queue:global_pipe_read)
         let newHandleState = PipeReader.HandleState(handle:handle, callback: queue, handler:handler, source: newSource, capture: intakeQueue)
         newSource.setEventHandler(handler: { [handle, newHandleState] in
-            if let newData = handle.availableData() {
-                newHandleState.intakeData(newData)
-            }
+            handle.availableDataLoop({ [newHandleState] (someData) in
+                print(Colors.dim(" \(handle) -> \(someData?.count) bytes"))
+                if let validateData = someData {
+                    newHandleState.intakeData(validateData)
+                }
+            })
         })
         accessModify({
             self.handles[handle] = newHandleState
