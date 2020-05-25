9b9b58cf7cc2a3987f73e17f95b8742217885b74
Tanner Silva <t@tannersilva.com>
2020-05-25 00:22:02 -0500

    removed relative priority extensions

--- Sources/TToolkit/SwiftSlash/Queues.swift
+++ Sources/TToolkit/SwiftSlash/Queues.swift
@@ -6,44 +6,6 @@ internal let global_lock_queue = DispatchQueue(label:"com.tannersilva.global.pro
 internal let global_pipe_read = DispatchQueue(label:"com.tannerdsilva.global.process.handle.read", qos:maximumPriority, attributes:[.concurrent])
 internal let global_run_queue = DispatchQueue(label:"com.tannerdsilva.global.process.launch", target:process_master_queue)
 
-extension Priority {
-	internal var process_launch_priority:DispatchQoS {
-		get {
-            return self.asDispatchQoS(relative:-15)
-		}
-	}
-	
-	internal var process_async_priority:DispatchQoS {
-		get {
-			return self.asDispatchQoS(relative:15)
-		}
-	}
-	
-	internal var process_reading_fast_capture_priority:DispatchQoS {
-		get {
-			return self.asDispatchQoS(relative:Int(Int32.max))
-		}
-	}
-	
-	internal var process_reading_priority:DispatchQoS {
-		get {
-			return self.asDispatchQoS(relative:100)
-		}
-	}
-	
-	internal var process_writing_priority:DispatchQoS {
-		get {
-			return self.asDispatchQoS(relative:50)
-		}
-	}
-    
-    internal var process_terminate_priority:DispatchQoS {
-        get {
-            return self.asDispatchQoS(relative:300)
-        }
-    }
-}
-
 internal let global_pipe_lock = DispatchQueue(label:"com.tannersilva.global.pipe-init.sync", target:global_lock_queue)
 
 //this queue is assigned a priority since it will be passed to a dispatchsource
