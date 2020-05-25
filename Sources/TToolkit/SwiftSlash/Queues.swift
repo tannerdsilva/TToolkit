bd879f09e31e36386d2f216e65d61ccabfe2c241
Tanner Silva <t@tannersilva.com>
2020-05-25 00:24:31 -0500

    removed comments from priorities queue

--- Sources/TToolkit/SwiftSlash/Queues.swift
+++ Sources/TToolkit/SwiftSlash/Queues.swift
@@ -8,13 +8,4 @@ internal let global_run_queue = DispatchQueue(label:"com.tannerdsilva.global.pro
 
 internal let global_pipe_lock = DispatchQueue(label:"com.tannersilva.global.pipe-init.sync", target:global_lock_queue)
 
-//this queue is assigned a priority since it will be passed to a dispatchsource
-
 internal let process_launch_async_fast = DispatchQueue(label:"com.tannersilva.global.process.launch-serial")
-//internal let process_intialize_serial = DispatchQueue(label:"com.tannersilva.global.process.initialize-serial", target:process_master_queue)
-//
-//internal let g_process_launch_queue = DispatchQueue(label:"com.tannersilva.instance.process.launch", qos:maximumPriority, target:process_master_queue)
-//internal let g_process_read_queue = DispatchQueue(label:"com.tannersilva.instance.process.read", qos:maximumPriority, target:process_master_queue)
-//internal let g_process_write_queue = DispatchQueue(label:"com.tannersilva.instance.process.write", qos:maximumPriority, target:process_master_queue)
-//internal let g_process_callback_queue = DispatchQueue(label:"com.tannersilva.instance.process.callback", qos:maximumPriority, target:process_master_queue)
-//
