d81d8a93b325b018ad75e2d539a9e223832ff1da
Tanner Silva <t@tannersilva.com>
2020-05-30 18:44:33 -0500

    simplified explode to be better utilized against the swift interpreter

--- Sources/TToolkit/Asynchronous/Explode.swift
+++ Sources/TToolkit/Asynchronous/Explode.swift
@@ -8,125 +8,166 @@
 import Foundation
 
 //explosions allow for multi-threaded collection mapping
-fileprivate let _explodeGlobal = DispatchQueue(label:"com.tannersilva.function.explode.merge", attributes:[.concurrent])
+fileprivate let _explodeGlobal = DispatchQueue(label:"com.tannerdsilva.global.function.explode", attributes:[.concurrent])
+
+//extension Collection {
+//    fileprivate func sequenceBuffer<R>(_ work:@escaping(UnsafeBufferPointer<Element>) throws -> R) rethrows -> R {
+//		let buffer = UnsafeMutableBufferPointer<Element>.allocate(capacity: self.count)
+//        _ = buffer.initialize(from: self)
+//		defer {
+//			buffer.deallocate()
+//		}
+//		return try work(UnsafeBufferPointer(buffer))
+//	}
+//
+//	public func explode(using thisFunction:@escaping (Int, Element) throws -> Void) {
+//		self.sequenceBuffer { unsafeBuff in
+//			print("O? \(unsafeBuff.count)")
+//			unsafeBuff._explode_v(using:thisFunction)
+//		}
+//	}
+//	
+//	public func explode<T>(using thisFunction:@escaping (Int, Element) -> T?, merge mergeFunction:@escaping (Int, T) -> Void) {
+//		self.sequenceBuffer { unsafeBuff in
+//			print("O? \(unsafeBuff.count)")
+//			unsafeBuff._explode_v_merge(using:thisFunction, merge:mergeFunction)
+//		}
+//	}
+//	
+//	public func explode<T>(using thisFunction:@escaping (Int, Element) -> T?) -> Set<T> where T:Hashable {
+//		return self.sequenceBuffer { unsafeBuff in
+//			print("O? \(unsafeBuff.count)")
+//			return unsafeBuff._explode_r_set(using:thisFunction)
+//		} ?? Set<T>()
+//	}
+//	
+//	public func explode<T, U>(using thisFunction:@escaping (Int, Element) -> (key:T, value:U)) -> [T:U] where T:Hashable {
+//		return self.sequenceBuffer { unsafeBuff in
+//			print("O? \(unsafeBuff.count)" )
+//			return unsafeBuff._explode_r_dict(using:thisFunction)
+//		} ?? [T:U]()
+//	}
+//}
 
 extension Collection {
-    fileprivate func sequenceBuffer<R>(_ work:@escaping(UnsafeBufferPointer<Element>) throws -> R) rethrows -> R {
-		let buffer = UnsafeMutableBufferPointer<Element>.allocate(capacity: self.count)
-        _ = buffer.initialize(from: self)
-		defer {
-			buffer.deallocate()
-		}
-		return try work(UnsafeBufferPointer(buffer))
-	}
-
-	public func explode(using thisFunction:@escaping (Int, Element) throws -> Void) {
-		self.sequenceBuffer { unsafeBuff in
-			print("O? \(unsafeBuff.count)")
-			unsafeBuff._explode_v(using:thisFunction)
-		}
-	}
-	
-	public func explode<T>(using thisFunction:@escaping (Int, Element) -> T?, merge mergeFunction:@escaping (Int, T) -> Void) {
-		self.sequenceBuffer { unsafeBuff in
-			print("O? \(unsafeBuff.count)")
-			unsafeBuff._explode_v_merge(using:thisFunction, merge:mergeFunction)
-		}
-	}
-	
-	public func explode<T>(using thisFunction:@escaping (Int, Element) -> T?) -> Set<T> where T:Hashable {
-		return self.sequenceBuffer { unsafeBuff in
-			print("O? \(unsafeBuff.count)")
-			return unsafeBuff._explode_r_set(using:thisFunction)
-		} ?? Set<T>()
-	}
-	
-	public func explode<T, U>(using thisFunction:@escaping (Int, Element) -> (key:T, value:U)) -> [T:U] where T:Hashable {
-		return self.sequenceBuffer { unsafeBuff in
-			print("O? \(unsafeBuff.count)" )
-			return unsafeBuff._explode_r_dict(using:thisFunction)
-		} ?? [T:U]()
-	}
-}
-
-
-extension UnsafeBufferPointer {
-	//explode - no return values	
-	fileprivate func _explode_v(using thisFunction:@escaping (Int, Element) throws -> Void) {
-		guard let startIndex = baseAddress else {
-			return
-		}
-		let launchSem = DispatchSemaphore(value:ProcessInfo.processInfo.activeProcessorCount)
-		DispatchQueue.concurrentPerform(iterations:count) { n in
-			try? thisFunction(n, startIndex.advanced(by:n).pointee)
-		}
-	}
-
-	//explode a collection - allows the user to handle the merging of data themselves.
-	//return values of the primary `explode` block are passed to a serial thread where the user can handle the data as necessary
-	fileprivate func _explode_v_merge<T>(using thisFunction:@escaping (Int, Element) throws -> T?, merge mergeFunction:@escaping (Int, T) throws -> Void) {
-		guard let startIndex = baseAddress else {
-			return
-		}
+	//explode a collection - no return values
+    public func explode(using thisFunction:@escaping (Int, Element) throws -> Void) {
+        guard count > 0 else {
+        	return
+        }
         
-        let launchSem = DispatchSemaphore(value:ProcessInfo.processInfo.activeProcessorCount)
-        let mergeQueue = DispatchQueue(label:"com.tannersilva.function.explode.merge", target:Priority.`default`.globalConcurrentQueue)
-		DispatchQueue.concurrentPerform(iterations:count) { n in
-			launchSem.wait()
-			defer {
-				launchSem.signal()
-			}
-			if let returnedValue = try? thisFunction(n, startIndex.advanced(by:n).pointee) {
-				mergeQueue.sync {
-					try? mergeFunction(n, returnedValue)
-				}
-			}
-		}
-	}
-
-	//explode a collection - returns a set of hashable objects
-	fileprivate func _explode_r_set<T>(using thisFunction:@escaping (Int, Element) throws -> T?) -> Set<T> where T:Hashable {
-		guard let startIndex = baseAddress else {
-			return Set<T>()
-		}
-		let launchSem = DispatchSemaphore(value:ProcessInfo.processInfo.activeProcessorCount)
-		var buildData = Set<T>()
-		let callbackQueue = DispatchQueue(label:"com.tannersilva.function.explode.merge", target:Priority.`default`.globalConcurrentQueue)
-		DispatchQueue.concurrentPerform(iterations:count) { n in
-			launchSem.wait()
-			defer {
-				launchSem.signal()
-			}
-			if let returnedValue = try? thisFunction(n, startIndex.advanced(by:n).pointee) {
-				callbackQueue.sync {
-					buildData.update(with:returnedValue)
-				}
-			}
-		}
-		return buildData
-	}
-
-	//explode a collection - returns a dictionary
-	fileprivate func _explode_r_dict<T, U>(using thisFunction:@escaping (Int, Element) throws -> (key:T, value:U)) -> [T:U] where T:Hashable {
-		guard let startIndex = baseAddress else {
-			return [T:U]()
-		}
-		let launchSem = DispatchSemaphore(value:ProcessInfo.processInfo.activeProcessorCount)
-		var buildData = [T:U]()
-		let callbackQueue = DispatchQueue(label:"com.tannersilva.function.explode.merge", target:Priority.`default`.globalConcurrentQueue)
-		DispatchQueue.concurrentPerform(iterations:count) { n in
-			launchSem.wait()
-			defer {
-			  launchSem.signal()
-			}
-			if let returnedValue = try? thisFunction(n, startIndex.advanced(by:n).pointee) {
-				if returnedValue.value != nil {
-					callbackQueue.sync {
-						buildData[returnedValue.key] = returnedValue.value
-					}
-				}
-			}
-		}
-		return buildData
-	}
+        let enumerateQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        let iterator = self.makeIterator()
+        func getNext() -> Self.Element? {
+        	return enumerateQueue.sync {
+        		return iterator.next()
+        	}
+        }
+        
+        DispatchQueue.concurrentPerform(iterations:count) { n in
+        	guard let thisItem = getNext() else {
+        		return
+        	}
+        	try? thisFunction(n, thisItem)
+        }
+    }
+    
+    //explode a collection - allows the user to handle the merging of data themselves
+    public func explode<T>(using thisFunction:@escaping (Int, Element) throws -> T?, merge mergeFunction:@escaping (Int, T) throws -> Void) {
+        guard count > 0 else {
+        	return
+        }
+        
+        //pre-processing access
+        let enumerateQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        let iterator = self.makeIterator()
+        func getNext() -> Self.Element? {
+        	return enumerateQueue.sync {
+        		return iterator.next()
+        	}
+        }
+        
+        //post-process merging
+        let returnQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        
+        //process
+        DispatchQueue.concurrentPerform(iterations:count) { n in
+        	guard let thisItem = getNext() else {
+        		return
+        	}
+        	if let returnedValue = try? thisFunction(n, thisItem) {
+        		returnQueue.sync {
+        			try? mergeFunction(n, returnedValue)
+        		}
+        	}
+        }
+    }
+    
+    //explode a collection - returns a set of hashable objects
+    public func explode<T>(using thisFunction:@escaping (Int, Element) throws -> T?) -> Set<T> where T:Hashable {
+        guard count > 0 else {
+        	return
+        }
+        
+        //pre-processing access
+        let enumerateQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        let iterator = self.makeIterator()
+        func getNext() -> Self.Element? {
+        	return enumerateQueue.sync {
+        		return iterator.next()
+        	}
+        }
+        
+        //post-process merging
+        let buildQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        var buildData = Set<T>()
+        
+        //process
+        DispatchQueue.concurrentPerform(iterations:count) { n in
+        	guard let thisItem = getNext() else {
+        		return
+        	}
+        	if let returnedValue = try? thisFunction(n, thisItem) {
+        		returnQueue.sync {
+        			buildData.update(with:returnedValue)
+        		}
+        	}
+        }
+        
+        return buildData
+    }
+    
+    //explode a collection - returns a dictionary
+    public func explode<T, U>(using thisFunction:@escaping (Int, Element) throws -> (key:T, value:U)) -> [T:U] where T:Hashable {
+        guard count > 0 else {
+        	return
+        }
+        
+        //pre-processing access
+        let enumerateQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        let iterator = self.makeIterator()
+        func getNext() -> Self.Element? {
+        	return enumerateQueue.sync {
+        		return iterator.next()
+        	}
+        }
+        
+        //post-process merging
+        let buildQueue = DispatchQueue(label:"com.tannersilva.function.explode.enumerate", target:_explodeGlobal)
+        var buildData = Set<T>()
+        
+        //process
+        DispatchQueue.concurrentPerform(iterations:count) { n in
+        	guard let thisItem = getNext() else {
+        		return
+        	}
+        	if let returnedValue = try? thisFunction(n, thisItem) {
+        		returnQueue.sync {
+        			buildData.update(with:returnedValue)
+        		}
+        	}
+        }
+        
+        return buildData
+    }
 }
