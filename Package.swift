f2d723bfb151c2b6718ec60e62ecb8c8667f42f8
Tanner Silva <t@tannersilva.com>
2020-07-19 20:23:07 -0500

    new Ctt_spawn version number

--- Package.swift
+++ Package.swift
@@ -17,15 +17,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    	.package(url: "https://github.com/IBM-Swift/Swift-SMTP.git", .upToNextMinor(from:"5.1.1"),
-    	.package(url: "https://github.com/tannerdsilva/Ctt_clone.git", .upToNextMinor(from:"1.0.1"))
+    	.package(url: "https://github.com/IBM-Swift/Swift-SMTP.git", .upToNextMinor(from:"5.1.1")),
+    	.package(url: "https://github.com/tannerdsilva/Ctt_spawn.git", .upToNextMinor(from:"1.0.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TToolkit",
-            dependencies: ["SwiftSMTP", "Ctt_clone"]),
+            dependencies: ["SwiftSMTP", "Ctt_spawn"]),
         .testTarget(
             name: "TToolkitTests",
             dependencies: ["TToolkit"]),
