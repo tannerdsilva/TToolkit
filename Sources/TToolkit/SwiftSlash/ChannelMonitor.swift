c5905948ce4287357c76109d14165beae903dbd2
tannerdsilva <t@tannersilva.com>
2020-10-06 18:17:21 +0000

    fixing things

--- Sources/TToolkit/SwiftSlash/ChannelMonitor.swift
+++ Sources/TToolkit/SwiftSlash/ChannelMonitor.swift
@@ -108,17 +108,15 @@ internal class DataChannelMonitor {
 						switch shouldParse {
 							case true:
 								//parse the data buffer
+								let parsedLines = self.dataBuffer.cutLines(flush:terminate)
 								if self.fh == 5 && terminate {
 									if let terminateString = String(data:self.dataBuffer, encoding:.utf8) {
 										print(Colors.magenta("the data buffer has \(self.dataBuffer.count) bytes waiting to be parsed '\(terminateString)'"))
 									} else {
 										print(Colors.magenta("something went really wrong"))
-									}
-									
-									
+									}	
 									return
 								}
-								let parsedLines = self.dataBuffer.cutLines(flush:terminate)
 								if parsedLines.lines != nil && parsedLines.lines!.count != 0 {
 									//synchronize and determine if a callback has already been scheduled
 									self.internalSync.sync {
