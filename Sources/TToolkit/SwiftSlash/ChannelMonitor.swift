45ab1be4b37b2e91abfeb13a922128125877085c
tannerdsilva <t@tannersilva.com>
2020-10-06 16:25:34 +0000

    working on fixes

--- Sources/TToolkit/SwiftSlash/ChannelMonitor.swift
+++ Sources/TToolkit/SwiftSlash/ChannelMonitor.swift
@@ -79,10 +79,12 @@ internal class DataChannelMonitor {
 						print("(C){\(self.fh)} [\(capturedData.count)]")
 					}
 				} catch FileHandleError.error_again {
+					print(Colord.dim("[\(self.fh)] ! DONE !"))
 				} catch FileHandleError.error_wouldblock {
+					print(Colord.dim("[\(self.fh)] ! DONE !"))
 				} catch FileHandleError.error_pipe {
 					if (terminate == false) {
-						print(Colors.Red("[\(self.fh)] ERROR PIPE"))
+						print(Colors.Red("[\(self.fh)] ERROR PIPE (captured \(self.dataBuffer.count) bytes in this iteration)"))
 					}
 				} catch let error {
 					print(Colors.Red("IO ERROR: \(error)"))
