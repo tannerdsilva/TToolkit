e30f276fbedda60d09b60878a194ca7484fc4e1b
Tanner Silva <t@tannersilva.com>
2020-11-24 01:56:00 +0000

    Removed shell framework from TToolkit. See tannerdsilva/SwiftSlash for a more complete solution

--- Sources/TToolkit/Extensions/FileManager.swift
+++ Sources/TToolkit/Extensions/FileManager.swift
@@ -1,15 +1,10 @@
 import Foundation
 
 extension FileManager {
-     public func mktemp(clashVerify:Bool = false) throws -> URL {
+     public func mktemp() throws -> URL {
     	let temporaryDirectory = try FileManager.default.temporaryDirectory
     	let dirName = String.random(length:Int.random(in:10..<24))
     	let targetURL = temporaryDirectory.appendingPathComponent(dirName, isDirectory:true)
-    	if clashVerify {
-    		if try FileManager.default.fileExists(atPath:targetURL.path) == true {
-    			throw CommandError.temporaryDirectoryNameConflict
-    		}
-    	}
 		try FileManager.default.createDirectory(at:targetURL, withIntermediateDirectories:false)
 		return targetURL
     }
--- Sources/TToolkit/Host/Host.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum ShellAuthentication {
-	case password(String)
-	case privateKey(URL)
-}
